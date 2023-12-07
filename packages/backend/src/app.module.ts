272231c1c3e02ebbfa0d0df739a07e57276bb2de
Paula Arce <118852959+Paula-A21@users.noreply.github.com>
2023-12-07 19:40:55 -0300

    Adding configs

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongodbModule } from './mongodb/mongodb.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [MongodbModule],
+  imports: [MongodbModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
